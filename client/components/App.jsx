493d2ed45aef924fe5d1067a70af56a5fbc6526a
Adam Whitman <emagdaeh@yahoo.com>
2021-03-29 22:21:02 -0700

    Working on hidden displays

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,28 +6,79 @@ const App = () => {
   const [player2, setPlayer2] = useState('');
   const [firstPlayerInfo, setFirstPlayerInfo] = useState('');
   const [secondPlayerInfo, setSecondPlayerInfo] = useState('');
+  const [dataTest, setDataTest] = useState('');
 
   const handleCompare = (event) => {
     event.preventDefault();
 
-    axios
-      .get('/api/playersInfo')
-      .then(results)
-      .catch();
+    // axios
+    //   .get('/api/playersInfo')
+    //   .then((results) => {
+    //     console.log(results);
+    //     setFirstPlayerInfo('Marsh');
+    //   })
+    //   .catch(console.log);
+
+    setFirstPlayerInfo('Tested true');
+    setSecondPlayerInfo('Tested true too');
+  };
+
+  const handleFavorite = (event) => {
+    event.preventDefault();
+
+    // const player = event.target;
+
+    // console.log('player: ', player);
+
+    // axios
+    //   .post('/api/favoritePlayer', {})
+    //   .then((status) => {
+    //     console.log('Status: ', status);
+    //   })
+    //   .catch((error) => {
+    //     console.log('Error: ', error);
+    //   });
+
+    // setPlayer1('');
+    // setPlayer2('');
   };
 
   return (
     <div>
       <form>
         Player 1:
-        <input type="text" name={player1} placeholder="Marshawn Lynch" onChange={event => setPlayer1(event.target.value)} />
+        <input type="text" name={player1} placeholder="Marshawn Lynch" onChange={(event) => setPlayer1(event.target.value)} />
       </form>
       <button type="submit" onClick={handleCompare}>Get Player 1 Stats</button>
+      <button type="submit" onClick={handleFavorite}>Add Player to Roster</button>
+      {(firstPlayerInfo !== '') ? (
+        <div>
+          Current Team:
+          <div>Team</div>
+          Injury Rate Per Season:
+          <div>Person</div>
+          Injury Rate for Current Team:
+          <div>Bad Team</div>
+        </div>
+      )
+        : null}
       <form>
         Player 2:
-        <input type="text" name={player2} placeholder="Jerry Rice" onChange={event => setPlayer2(event.target.value)} />
+        <input type="text" name={player2} placeholder="Jerry Rice" onChange={(event) => setPlayer2(event.target.value)} />
       </form>
       <button type="submit" onClick={handleCompare}>Get Player 2 Stats</button>
+      <button type="submit" onClick={handleFavorite}>Add Player to Roster</button>
+      {(secondPlayerInfo !== '') ? (
+        <div>
+          Current Team:
+          <div>Team</div>
+          Injury Rate Per Season:
+          <div>Person</div>
+          Injury Rate for Current Team:
+          <div>Bad Team</div>
+        </div>
+      )
+        : null}
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -12,6 +12,15 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/api/playersInfo', (req, res) => {
+  axios
+    .post('https://api.nfl.com/oauth/token')
+    .then((results) => res.send(results))
+    .catch((error) => {
+      console.log('Error: ', error);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Reviews service listening at http://localhost:${port}`);
 });
